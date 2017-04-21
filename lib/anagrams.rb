9fa0eb03045f3cb35d73265e5b9ae1509741b0b1
Dominic Brown <yeahdom@gmail.com>
2017-04-21 13:58:09 -0700

    fix valid word checker

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,16 +3,16 @@ def ana(word1, word2)
   verdict = "no verdict"
   vowels = ["a","e","i","o","u","y"]
 
+  # create arrays that preserve the word structure
+  # words1_arr = word1.downcase().split(" ").select { |letter| letter =~ /[a-z]/ }
+  # words2_arr = word1.downcase().split(" ").select { |letter| letter =~ /[a-z]/ }
+
   # create arrays that:  reduce case, eliminate spaces and punctuation
   letters1_arr = word1.downcase().split("").select { |letter| letter =~ /[a-z]/ }
   letters2_arr = word2.downcase().split("").select { |letter| letter =~ /[a-z]/ }
 
-  # create arrays that preserve the word structure
-  # words1_arr =
-  # words2_arr =
-
   # check words are valid
-  if ((letters1_arr & vowels).empty?) || ((letters2_arr & vowels).empty?)
+  if (((word1.split("") & vowels).empty?) || ((word2.split("") & vowels).empty?))
     verdict = "I don't think those are all real words you silly mongoose... try again!"
   elsif (((letters1_arr & letters2_arr).length) == letters1_arr.length) # check 2 words for anagram
       verdict = "Cool, your words are an anagram!"
