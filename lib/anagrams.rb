b4c0a0e233fc8d882a74615a62740ca8697ac5a2
Dominic Brown <yeahdom@gmail.com>
2017-04-21 10:43:49 -0700

    refactor to check for vowels against an array

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,15 +1,17 @@
 class String
   define_method(:ana) do
     verdict = ""
-    # vowels = ["a","e","i","o","u","y"]
-    # "string".index?(/\a/i) => nil
-
-    if (self.include?("a") || self.include?("e") || self.include?("i") || self.include?("o") || self.include?("u") || self.include?("y"))
+    vowels = ["a","e","i","o","u","y"]
+    if (self.split("") & vowels).empty?
+      verdict = "I don't think those are all real words you silly mongoose... try again!"
+    elsif
       verdict = "yup, those are words"
     else
-      verdict = "I don't think those are all real words you silly mongoose... try again!"
+      verdict = "errors yo"
     end
-
     return verdict
   end
 end
+
+
+# "string".index?(/\a/i) => nil
