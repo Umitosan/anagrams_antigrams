03471f4de9a323137ec2745761767aa03a1c1571
Dominic Brown <yeahdom@gmail.com>
2017-04-21 11:06:46 -0700

    add second unit-test spec, refactor 'ana' method to stand alone and take multiple args

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,17 +1,17 @@
-class String
-  define_method(:ana) do
-    verdict = ""
-    vowels = ["a","e","i","o","u","y"]
-    if (self.split("") & vowels).empty?
-      verdict = "I don't think those are all real words you silly mongoose... try again!"
-    elsif
-      verdict = "yup, those are words"
-    else
-      verdict = "errors yo"
-    end
-    return verdict
+
+def ana(word1, word2)
+  verdict = ""
+  vowels = ["a","e","i","o","u","y"]
+  if ((word1.split("") & vowels).empty?)
+    verdict = "I don't think those are all real words you silly mongoose... try again!"
+  elsif
+    verdict = "yup, those are words"
+  else
+    verdict = "errors yo"
   end
+  return verdict
 end
+
 
 
 # "string".index?(/\a/i) => nil
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,8 +3,16 @@ require('anagrams')
 
 describe('String#ana') do
 
-  it("checks to see if all the user's words have 1 or more vowels") do
-   expect("qwrt".ana()).to(eq("I don't think those are all real words you silly mongoose... try again!"))
+  it("checks to see if the user's word have 1 or more vowels") do
+   expect(ana("qwrt", "")).to(eq("I don't think those are all real words you silly mongoose... try again!"))
   end
+
+  it("checks to see if both the user's words have 1 or more vowels") do
+   expect(ana("real", "qwrt")).to(eq("I don't think those are all real words you silly mongoose... try again!"))
+  end
+
+  # it("checks to see if all the user's words are anagrams") do
+  #  expect(ana("","")).to(eq("Cool, your words are anagrams!"))
+  # end
 
 end
