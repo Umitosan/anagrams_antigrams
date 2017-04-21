f4993a577ba248c5aa2d74f03666ba5452260617
Dominic Brown <yeahdom@gmail.com>
2017-04-21 16:38:06 -0700

    add basic code for front end, add integration test

--- app.rb
+++ app.rb
@@ -7,3 +7,8 @@ require('./lib/anagrams.rb')
 get('/') do
   erb(:form1)
 end
+
+get('/check') do
+  @final_verdict = params_fetch(ana('phrase1', 'phrase2'))
+  erb(:output)
+end
--- spec/anagrams_integration_spec.rb
+++ spec/anagrams_integration_spec.rb
@@ -1,13 +1,15 @@
 require('capybara/rspec')
 require('./app')
+require('launchy')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe('', {:type => :feature}) do
-#   it('processes...') do
-#     visit('/')
-#     fill_in('@var', :with => 'string')
-#     click_button('buttonname')
-#     expect(page).to have_content('string')
-#   end
-# end
+describe('the form path', {:type => :feature}) do
+  it('processes the users phrases and returns the findings') do
+    visit('/')
+    fill_in('phrase1', :with => 'listen')
+    fill_in('phrase2', :with => 'silent')
+    click_button('Analize')
+    expect(page).to have_content('Cool, your words are an anagram!')
+  end
+end
