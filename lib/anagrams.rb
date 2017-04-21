065efeab0145e9020cb19273c02b6228841cd273
Dominic Brown <yeahdom@gmail.com>
2017-04-21 11:50:12 -0700

    add code to downcase method args

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,6 +3,10 @@ def ana(word1, word2)
   verdict = "no verdict"
   vowels = ["a","e","i","o","u","y"]
 
+  # reduce all chars to lower case
+  word1 = word1.downcase()
+  word2 = word2.downcase()
+
   # check both inputs are valid words
   if (((word1.split("") & vowels).empty?) || ((word2.split("") & vowels).empty?))
     verdict = "I don't think those are all real words you silly mongoose... try again!"
