992492e856c901911a3bff1ada90529f59f759cc
Dominic Brown <yeahdom@gmail.com>
2017-04-21 11:08:03 -0700

    add code to pass spec using 2 words

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,7 +2,7 @@
 def ana(word1, word2)
   verdict = ""
   vowels = ["a","e","i","o","u","y"]
-  if ((word1.split("") & vowels).empty?)
+  if (((word1.split("") & vowels).empty?) || ((word2.split("") & vowels).empty?))
     verdict = "I don't think those are all real words you silly mongoose... try again!"
   elsif
     verdict = "yup, those are words"
