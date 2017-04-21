a06f1d8f4cae630f775b2d4458fb92a9a47dd306
Dominic Brown <yeahdom@gmail.com>
2017-04-21 12:20:19 -0700

    add code to ignore punctuation and whitespace

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,15 +3,15 @@ def ana(word1, word2)
   verdict = "no verdict"
   vowels = ["a","e","i","o","u","y"]
 
-  # reduce all chars to lower case
-  word1 = word1.downcase()
-  word2 = word2.downcase()
+  # convert to arrays, handle case, handle spaces and punctuation
+  word1_arr = word1.downcase().split("").select { |letter| letter =~ /[a-z]/ }
+  word2_arr = word2.downcase().split("").select { |letter| letter =~ /[a-z]/ }
 
   # check both inputs are valid words
-  if (((word1.split("") & vowels).empty?) || ((word2.split("") & vowels).empty?))
+  if ((word1_arr & vowels).empty?) || ((word2_arr & vowels).empty?)
     verdict = "I don't think those are all real words you silly mongoose... try again!"
   else # simple check for anagrams between two words
-    if (((word1.split("") & word2.split("")).length) == word1.length)
+    if (((word1_arr & word2_arr).length) == word1_arr.length)
       verdict = "Cool, your words are an anagram!"
     else
       verdict = "Sorry, that's not an anagram"
