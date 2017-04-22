f53c5d3c1ba73011c23b87072621bcd41c8ed022
Dominic Brown <yeahdom@gmail.com>
2017-04-21 17:00:26 -0700

    add more front end changes, still not working

--- app.rb
+++ app.rb
@@ -2,13 +2,17 @@ require('sinatra')
 require('sinatra/reloader')
 require('pry')
 require('rspec')
-require('./lib/anagrams.rb')
+require('./lib/anagrams')
+also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:form1)
 end
 
-get('/check') do
-  @final_verdict = params_fetch(ana('phrase1', 'phrase2'))
+get('/analize') do
+  # @phrase1 = params_fetch('phrase1')
+  # @phrase2 = params_fetch('phrase2')
+  # @findings = ana(@phrase1, @phrase2)
+  @findings = ana(params_fetch('phrase1'), params_fetch('phrase2'))
   erb(:output)
 end
