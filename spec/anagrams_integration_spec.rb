05441641ee32eb5de33687a5d338487af7cb39bc
Dominic Brown <yeahdom@gmail.com>
2017-04-21 17:41:38 -0700

    update integration test to reflect front end changes

--- spec/anagrams_integration_spec.rb
+++ spec/anagrams_integration_spec.rb
@@ -1,15 +1,15 @@
-# require('capybara/rspec')
-# require('./app')
-# require('launchy')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('the form path', {:type => :feature}) do
-#   it('processes the users phrases and returns the findings') do
-#     visit('/')
-#     fill_in('phrase1', :with => 'listen')
-#     fill_in('phrase2', :with => 'silent')
-#     click_button('analize')
-#     expect(page).to have_content('Cool, your words are an anagram!')
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+require('launchy')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the form path', {:type => :feature}) do
+  it('processes the users phrases and returns the findings') do
+    visit('/')
+    fill_in('phrase1', :with => 'listen')
+    fill_in('phrase2', :with => 'silent')
+    click_button('GO')
+    expect(page).to have_content('Cool, your words are an anagram!')
+  end
+end
